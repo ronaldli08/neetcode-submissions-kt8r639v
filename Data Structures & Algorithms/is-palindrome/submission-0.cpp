533facfa3db5a7bcc9e17b0108d9c037731fb360
Ronald Li <ronaldli@berkeley.edu>
2026-06-08 23:23:41 -0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int p1 = 0;
+        int p2 = s.size() - 1;
+        while(p1 < p2) {
+            if(!isalnum(s[p1])) {
+                ++p1;
+                continue;
+            }
+            if(!isalnum(s[p2])) {
+                --p2;
+                continue;
+            }
+            if(tolower(s[p1]) != tolower(s[p2])) return false;
+            ++p1;
+            --p2;
+        }
+        return true;
+    }
+};
