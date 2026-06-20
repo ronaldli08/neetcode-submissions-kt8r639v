1f486104d352bfd0336fa2d3895f9e8e21462103
Ronald Li <ronaldli@berkeley.edu>
2026-06-20 13:08:34 -0600

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        char cur = s[0];
+        int n = s.size();
+        if(n == 1) return 1;
+        int left = 0;
+        int right = 1;
+        vector<int> alphabet(26, 0);
+        alphabet[cur - 'A']++;
+        int maxFreq = 1;
+        int longest = 0;
+
+        while(right < n) {
+            alphabet[s[right] - 'A']++;
+            maxFreq = max(maxFreq, alphabet[s[right]-'A']);
+            if(right - left + 1 - maxFreq > k) {
+                alphabet[s[left] - 'A']--;
+                left++;
+            }
+            longest = right - left + 1;
+            right++;
+        }
+        return longest;
+    }
+};
